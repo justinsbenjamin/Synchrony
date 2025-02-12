a4246de29b70dcd3d0033926e2e95529bf5d8234
justinsbenjamin <justinbenjamin67@gmail.com>
2025-02-12 15:33:14 -0500

    more stuff

--- Synchrony.R
+++ Synchrony.R
@@ -15,29 +15,57 @@ library(tidyr)
 library(readxl)
 library(lubridate)
 
-# Read in data file
-data <- read_excel("GRD_data_2025.xlsx")
-View(data)
+# Read in data files
+nest_data <- read_excel("GRD_data_2025.xlsx")
+chick_data <- read_excel("Chick_GRD_2025.xlsx")
+View(nest_data)
+View(chick_data)
 
-data_filtered <- data %>% filter(is.na(Exclusion) | Exclusion == "") %>%
+# Filter nests that aren't excluded
+nest_data_filtered <- nest_data %>% filter(is.na(Exclusion) | Exclusion == "") %>%
   mutate(unique_nest_ID = paste(Year, Nest_ID, sep = "_"))
-View(data_filtered)
+View(nest_data_filtered)
 
-hatch_spread <- data_filtered %>%
+chick_data_filtered <- chick_data %>% filter(is.na(Exclusion) | Exclusion == "") %>%
+  mutate(unique_nest_ID = paste(Year, Nest_ID, sep = "_"))
+View(chick_data_filtered)
+
+# Calculating hatch spread
+hatch_spread <- nest_data_filtered %>%
   filter(Hatch_begin != "NA" & Hatch_end != "NA") %>%
   mutate(
     Hatch_begin = as.numeric(Hatch_begin),
     Hatch_end = as.numeric(Hatch_end),
     Hatch_spread = Hatch_end - Hatch_begin + 1)
 
-single_female <- data_filtered %>% filter(Clutch_size <= 5)
-joint_female <- data_filtered %>% filter(Clutch_size >= 6)
+# Separating single and joint female nests
+single_female <- nest_data_filtered %>% filter(Clutch_size <= 5)
+joint_female <- nest_data_filtered %>% filter(Clutch_size >= 6)
 
 single_hatch_spread <- hatch_spread %>% filter(Clutch_size <= 5)
 joint_hatch_spread <- hatch_spread %>% filter(Clutch_size >= 6)
 
 summary(single_hatch_spread$Hatch_spread)
 summary(joint_hatch_spread$Hatch_spread)
+
+
+
+
+
+
+# Assuming df1 has the clutch sizes and df2 has the nest information
+chick_data_w_clutch <- chick_data_filtered %>%
+  left_join(nest_data_filtered, by = "Nest_ID")
+
+# Remove duplicates in nest_data_filtered
+nest_data_filtered_unique <- nest_data_filtered %>%
+  distinct(Nest_ID, .keep_all = TRUE)
+
+# Perform the join
+chick_data_w_clutch <- chick_data_filtered %>%
+  left_join(nest_data_filtered_unique, by = "Nest_ID")
+
+
 
 
 # Example dataset (each row is an egg)
@@ -47,25 +75,30 @@ hatching_data <- data.frame(
 )
 
 # Calculate clutch size per nest
-hatch_summary <- hatching_data %>%
-  group_by(nest_id) %>%
-  mutate(clutch_size = n()) %>%
-  group_by(nest_id, hatching_day) %>%
-  summarise(n_hatched = n(), clutch_size = max(clutch_size), .groups = "drop") %>%
+hatch_summary <- chick_data_w_clutch %>%
+  group_by(Nest_ID, Hatch_day) %>%
+  summarise(n_hatched = n(), clutch_size = max(Clutch_size), .groups = "drop") %>%
   mutate(prop_hatched = n_hatched / clutch_size)
 
 # Aggregate proportions across all nests
 hatch_distribution <- hatch_summary %>%
-  group_by(hatching_day) %>%
+  group_by(Hatch_day) %>%
   summarise(avg_prop_hatched = mean(prop_hatched), .groups = "drop")
 
 # Plot
-ggplot(hatch_distribution, aes(x = hatching_day, y = avg_prop_hatched)) +
+ggplot(hatch_distribution, aes(x = Hatch_day, y = avg_prop_hatched)) +
   geom_col(fill = "skyblue", color = "black") +
   labs(x = "Hatching Day",
        y = "Mean Clutch Proportion") +
   theme_classic()
 
+
+ggplot(hatch_distribution, aes(x = Hatch_day, y = avg_prop_hatched)) +
+  geom_col(fill = "skyblue", color = "black") +
+  labs(x = "Hatching Day",
+       y = "Mean Clutch Proportion") +
+  scale_x_continuous(limits = c(0, 15))
+  theme_classic()
 
 
 
@@ -75,14 +108,10 @@ hatching_data <- data.frame(
   nest_ID = c("A", "B", "C", "A", "B", "C", "D"),
   hatching_day = c(1, 2, 3, 1, 1, 2, 3))
 
-# Create a unique nest identifier
-data_filtered <- data_filtered %>%
-  mutate(unique_nest_ID = paste(year, nest_ID, sep = "_"))
-
-print(hatching_data)
 
 
-# Do one histogram of clutch proportion for each type of nest
+
+
 
 
 
