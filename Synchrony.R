1fd04dbaa8991070eb276524c9de80d4222db763
justinsbenjamin <justinbenjamin67@gmail.com>
2025-02-13 13:23:28 -0500

    Fixed histogram

--- Synchrony.R
+++ Synchrony.R
@@ -1,8 +1,11 @@
 # Justin Benjamin
 # Synchrony Project
 
-# I am investigting hatching synchrony in Pūkeko (Porphyrio melanotus melanotus) 
-# as a possible evolutionary route to joint laying behaviour. 
+# I am investigting hatching synchrony in Pūkeko 
+# (Porphyrio melanotus melanotus) as a possible evolutionary route 
+# to joint laying behaviour. I will be exploring the hatching spread, 
+# hatching rate, and survival of single female and joint female nests 
+# and looking at the distribution of hatching. 
 
 # Load libraries
 library(dplyr)
@@ -23,11 +26,15 @@ View(chick_data)
 
 # Filter nests that aren't excluded
 nest_data_filtered <- nest_data %>% filter(is.na(Exclusion) | Exclusion == "") %>%
-  mutate(unique_nest_ID = paste(Year, Nest_ID, sep = "_"))
+  mutate(Nest_ID = paste(Year, Nest_ID, sep = "_")) %>%
+  mutate(Females = ifelse(Clutch_size <= 5, "Single Female", "Joint Females"))
 View(nest_data_filtered)
 
 chick_data_filtered <- chick_data %>% filter(is.na(Exclusion) | Exclusion == "") %>%
-  mutate(unique_nest_ID = paste(Year, Nest_ID, sep = "_"))
+  mutate(Nest_ID = paste(Year, Nest_ID, sep = "_")) %>%
+  left_join(nest_data_filtered %>% select(Nest_ID, Clutch_size, Hatched_eggs), by = "Nest_ID") %>%
+  mutate(Females = ifelse(Clutch_size <= 5, "Single Female", "Joint Females")) %>%
+  filter(!is.na(Females))
 View(chick_data_filtered)
 
 # Calculating hatch spread
@@ -38,75 +45,40 @@ hatch_spread <- nest_data_filtered %>%
     Hatch_end = as.numeric(Hatch_end),
     Hatch_spread = Hatch_end - Hatch_begin + 1)
 
-# Separating single and joint female nests
-single_female <- nest_data_filtered %>% filter(Clutch_size <= 5)
-joint_female <- nest_data_filtered %>% filter(Clutch_size >= 6)
 
-single_hatch_spread <- hatch_spread %>% filter(Clutch_size <= 5)
-joint_hatch_spread <- hatch_spread %>% filter(Clutch_size >= 6)
+data_proportions <- chick_data_filtered %>%
+  mutate(Hatch_proportion = Hatched_eggs / Clutch_size) %>%
+  filter(!is.na(Hatch_proportion)) %>% # Remove NAs if needed
+  group_by(Nest_ID)
 
-summary(single_hatch_spread$Hatch_spread)
-summary(joint_hatch_spread$Hatch_spread)
+data_filtered <- chick_data_filtered %>%
+  filter(!(Nest_ID %in% data_proportions))
 
-
-
-
-
-
-# Assuming df1 has the clutch sizes and df2 has the nest information
-chick_data_w_clutch <- chick_data_filtered %>%
-  left_join(nest_data_filtered, by = "Nest_ID")
-
-# Remove duplicates in nest_data_filtered
-nest_data_filtered_unique <- nest_data_filtered %>%
-  distinct(Nest_ID, .keep_all = TRUE)
-
-# Perform the join
-chick_data_w_clutch <- chick_data_filtered %>%
-  left_join(nest_data_filtered_unique, by = "Nest_ID")
+ggplot(data_proportions, aes(x = Hatch_day, weight = Hatch_proportion)) +
+  geom_histogram(fill = "skyblue", colour = "black", binwidth = 1) +
+  labs(x = "Relative Hatch Day", y = "Proportion of Eggs Hatched") +
+  facet_wrap(~ Females)
+  scale_y_continuous(breaks = seq(0, 120, by = 10)) +
+  theme_classic()
 
 
-
-
-# Example dataset (each row is an egg)
-hatching_data <- data.frame(
-  nest_id = c(1,1,1,2,2,2,2,3,3,3),  # Nest IDs
-  hatching_day = c(1,2,3,1,1,2,3,1,2,2)  # Hatching days
-)
-
-# Calculate clutch size per nest
-hatch_summary <- chick_data_w_clutch %>%
-  group_by(Nest_ID, Hatch_day) %>%
-  summarise(n_hatched = n(), clutch_size = max(Clutch_size), .groups = "drop") %>%
-  mutate(prop_hatched = n_hatched / clutch_size)
-
-# Aggregate proportions across all nests
-hatch_distribution <- hatch_summary %>%
-  group_by(Hatch_day) %>%
-  summarise(avg_prop_hatched = mean(prop_hatched), .groups = "drop")
 
 # Plot
-ggplot(hatch_distribution, aes(x = Hatch_day, y = avg_prop_hatched)) +
-  geom_col(fill = "skyblue", color = "black") +
+ggplot(chick_data_filtered, aes(x = Hatch_day, weight = Hatch_day)) +
+  geom_histogram(fill = "skyblue", colour = "black", binwidth = 1) +
+  facet_wrap(~ Females) +
   labs(x = "Hatching Day",
-       y = "Mean Clutch Proportion") +
-  theme_classic()
-
-
-ggplot(hatch_distribution, aes(x = Hatch_day, y = avg_prop_hatched)) +
-  geom_col(fill = "skyblue", color = "black") +
-  labs(x = "Hatching Day",
-       y = "Mean Clutch Proportion") +
-  scale_x_continuous(limits = c(0, 15))
-  theme_classic()
+       y = "Chicks hatched") +
+  scale_y_continuous(breaks = seq(0, 100, by = 10)) +
+  scale_x_continuous(breaks = seq(1, 12, by = 1)) +
+  theme_classic() +
+  theme(panel.spacing = unit(1, "lines"))
 
 
 
-# Example data set
-hatching_data <- data.frame(
-  year = c(2023, 2023, 2023, 2024, 2024, 2024, 2024),
-  nest_ID = c("A", "B", "C", "A", "B", "C", "D"),
-  hatching_day = c(1, 2, 3, 1, 1, 2, 3))
+
+
+
 
 
 
