b34655846e578b73fabae2660a23d4cf3e30a886
justinsbenjamin <justinbenjamin67@gmail.com>
2025-02-14 13:43:49 -0500

    Cleaned up code

--- Synchrony.R
+++ Synchrony.R
@@ -68,100 +68,6 @@ ggplot(chick_data_filtered, aes(x = Hatch_day, weight = Hatch_day)) +
 
 
 
-
-ggplot(chick_data_filtered, aes(x = Hatch_day, weight = Hatch_rate)) +
-  geom_histogram(fill = "skyblue", colour = "black", binwidth = 1) +
-  labs(x = "Relative Hatch Day", y = "Proportion of Eggs Hatched") +
-  facet_wrap(~ Females)
-scale_y_continuous(breaks = seq(0, 120, by = 10)) +
-  theme_classic()
-
-
-data_proportions <- chick_data_filtered %>%
-  group_by(Nest_ID) %>%
-  mutate(Hatch_proportion = (n() / Hatched_eggs) * 100) %>%  # Compute correct proportion
-  filter(!is.na(Hatch_proportion))  # Remove NAs if needed
-
-
-data_proportions <- chick_data_filtered %>%
-  mutate(Hatch_proportion = Hatched_eggs / Clutch_size) %>%
-  filter(!is.na(Hatch_proportion)) %>% # Remove NAs if needed
-  group_by(Nest_ID)
-
-data_filtered <- chick_data_filtered %>%
-  filter(!(Nest_ID %in% data_proportions))
-
-ggplot(data_filtered, aes(x = Hatch_day, y = data_proportions), 
-       fill = factor(Hatch_day)) +
-  geom_violin(alpha = 0.5) +  
-  geom_quasirandom(width = 0.2, size = 2, alpha = 0.7, color = "black", method = "smiley") + 
-  scale_y_continuous(labels = scales::percent_format(scale = 1), limits = c(0, 100)) +  
-  labs(x = "Hatching Day",
-       y = "Percentage of Clutch Hatched",
-       title = "Distribution of % Clutch Hatched Per Day") +
-  theme_classic() +
-  theme(legend.position = "none",
-        axis.text.x = element_text(size = 12),
-        axis.text.y = element_text(size = 12),
-        plot.title = element_text(hjust = 0.5, size = 14))
-
-
-
-# Compute proportion of clutch hatched per day
-
-data_proportions <- chick_data_filtered %>%
-  group_by(Nest_ID) %>%
-  mutate(Hatch_proportion = (n() / Hatched_eggs) * 100) %>%  # Compute correct proportion
-  filter(!is.na(Hatch_proportion))  # Remove NAs if needed
-
-# Plot violin graph
-ggplot(data_proportions, aes(x = as.factor(Hatch_day), y = Hatch_proportion)) +
-  geom_violin(fill = "lightblue", alpha = 0.5) +  
-  geom_jitter(width = 0.1, size = 2, alpha = 0.8) + 
-  scale_y_continuous(labels = scales::percent_format(scale = 1), limits = c(0, 100)) +  
-  scale_x_discrete(drop = FALSE) + 
-  labs(x = "Relative hatching day",
-       y = "Percentage of clutch hatched") +
-  theme_classic() +
-  theme(legend.position = "none",
-        axis.text.x = element_text(size = 12),
-        axis.text.y = element_text(size = 12),
-        plot.title = element_text(hjust = 0.5, size = 14))
-
-# Compute proportion of clutch hatched per day
-data_proportions <- chick_data_filtered %>%
-  mutate(Hatch_proportion = (1 / Hatched_eggs) * 100) %>%  # Convert to percentage
-  filter(!is.na(Hatch_proportion))  # Remove NAs if needed
-
-
-ggplot(data_proportions, aes(x = as.factor(Hatch_day), y = Hatch_proportion)) +
-  geom_violin(fill = "lightblue", alpha = 0.5) +  
-  geom_jitter(width = 0.1, size = 2, alpha = 0.8) +
-  scale_y_continuous(labels = scales::percent_format(scale = 1), limits = c(0, 60)) +  
-  scale_x_discrete(drop = FALSE) + 
-  labs(x = "Hatching Day",
-       y = "Percentage of Clutch Hatched",
-       title = "Distribution of % Clutch Hatched Per Day") +
-  theme_classic() +
-  theme(legend.position = "none",
-        axis.text.x = element_text(size = 12),
-        axis.text.y = element_text(size = 12),
-        plot.title = element_text(hjust = 0.5, size = 14))
-
-
-
-# Create the example dataset
-hatch_data <- data.frame(
-  Nest_ID = rep(c("Nest_1", "Nest_2", "Nest_3"), c(4, 4, 6)),  # Assign nests
-  Clutch_size = rep(c(8, 5, 10), c(4, 4, 6)),  # Repeat clutch sizes accordingly
-  Hatched_eggs = rep(c(4, 4, 6), c(4, 4, 6)),  # Repeat hatched eggs accordingly
-  Relative_hatch_day = c(1, 1, 1, 3,  # Nest 1
-                         1, 1, 1, 1,  # Nest 2
-                         1, 1, 2, 2, 5, 7)  # Nest 3
-)
-
-
-
 ###############################
 ##### This is the good figure!!!!
 ###############################
@@ -187,51 +93,8 @@ ggplot(data_proportions, aes(x = as.factor(Hatch_day), y = Hatch_proportion, gro
         plot.title = element_text(hjust = 0.5, size = 14)) +
   facet_wrap(~ Females, scales = "free_x")
 
-scale_x_(drop = FALSE) + 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-)
-
-# Calculate proportions for each nest and hatch day
-hatch_summary <- hatch_data %>%
-  group_by(Nest_ID, Relative_hatch_day) %>%
-  summarise(Hatched_count = n(), .groups = "drop") %>%
-  left_join(hatch_data %>% select(Nest_ID, Clutch_size) %>% distinct(), by = "Nest_ID") %>%
-  mutate(Proportion = (Hatched_count / Clutch_size) * 100)  # Convert to percentage
-
-# View summary dataset
-print(hatch_summary)
-
-ggplot(hatch_summary, aes(x = factor(Relative_hatch_day), y = Proportion, fill = factor(Relative_hatch_day))) +
-  geom_violin(alpha = 0.5) +  # Violin plot for density
-  geom_jitter(width = 0.1, size = 2, alpha = 0.8) +  # Add points for individual nests
-  scale_y_continuous(labels = scales::percent_format(scale = 1), limits = c(0, 100)) +  # Format y-axis as percentages
-  labs(x = "Hatching Day",
-       y = "Percentage of Eggs Hatched",
-       title = "Distribution of Hatching Proportions by Day") +
-  theme_minimal() +
-  theme(legend.position = "none",
-        axis.text.x = element_text(size = 12),
-        axis.text.y = element_text(size = 12),
-        plot.title = element_text(hjust = 0.5, size = 14))
 
 
 
